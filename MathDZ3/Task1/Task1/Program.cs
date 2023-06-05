04794630eb0ea0a01cbc7660a7abd373ebea8b12
Cyber-cringe <96581898+Cyber-cringe@users.noreply.github.com>
2023-06-05 22:40:59 +0300

    Add some more homework

---
+++ MathDZ3/Task1/Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Ввведите лимит");
+double limit = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Ввведите x");
+double x = Convert.ToDouble(Console.ReadLine());
+
+double lastpow = x * x;
+double lastdenomx = lastpow;
+double res = lastpow/lastdenomx + 1 ;
+
+for (int i = 2; i < 10; i++)
+{
+    double numerator = Math.Pow(lastpow, i);
+    double denominator = lastdenomx + Math.Pow(i, 3);
+    res += numerator / denominator;
+
+    if (Math.Abs(res) < limit) {break;}
+    lastpow = numerator;
+    lastdenomx = denominator;
+}
+
+Console.WriteLine("Результат: " + res);
---
+++ MathDZ3/Task1/Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Ввведите лимит");
+double limit = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Ввведите x");
+double x = Convert.ToDouble(Console.ReadLine());
+
+double lastnumer = x*x;
+double lastdenom = 1;
+double res = lastnumer / lastdenom;
+
+int Factorial(int n)
+{
+    if (n == 1) return 1;
+
+    return n * Factorial(n - 1);
+}
+
+
+for (int i = 1; i < 10; i++)
+{
+    double numerator = Math.Pow(lastnumer, (2*i - 1));
+    double denominator = Factorial((2*i)-1);
+    res += numerator / denominator;
+
+    if (Math.Abs(res) < limit) { break; }
+    lastnumer = numerator;
+
+
+}
+
+Console.WriteLine("Результат: " + res);
